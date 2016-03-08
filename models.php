88a91f0856d967f48c3c02c154ee3cbf1c885792
truongtn <truongtn@users.noreply.github.com>
2016-03-08 23:20:52 +0700

    Update models.php
    delete some line not necessary

--- models.php
+++ models.php
@@ -1,6 +1,1 @@
-<?php
-function connectdb(){
-    $conn=mysql_connect(DBHOST,DBUSER,DBPASS) or die("can't connect this database");
-    mysql_select_db(DBNAME,$conn);
-    return $conn;
-}
+//ORMs here
