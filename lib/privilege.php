619f0b6d1935060295dadfb71769510486754987
truongtn <truongtn@users.noreply.github.com>
2016-03-09 22:56:13 +0700

    Update privilege.php
    add set_role() and renme $number to $level

--- lib/privilege.php
+++ lib/privilege.php
@@ -1,9 +1,13 @@
-<?php
-function require_access($number){
+ <?php
+function require_access($level){
     if(session_status()!=PHP_SESSION_ACTIVE) session_start();
-    if(@(int)$_SESSION['role']>=$number){
+    if(@(int)$_SESSION['role']>=$level){
         return 0;
     }else{
         die('403');
     }
+}
+function set_role($level){
+    $_SESSION['role'] = $level;
+    return 0;
 }
